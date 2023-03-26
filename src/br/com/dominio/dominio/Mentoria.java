16590435a9cf4dfc46975a668a61570023f4a89a
hugokhesley <98715012+hugokhesley@users.noreply.github.com>
2023-03-26 04:24:45 -0300

    Mentoria e Curso

---
+++ src/br/com/dominio/dominio/Mentoria.java
@@ -0,0 +1,46 @@
+package br.com.dominio.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria {
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    private String titulo;
+    private String descricao;
+    private LocalDate data;
+
+    public Mentoria(){
+
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", data=" + data +
+                '}';
+    }
+}
